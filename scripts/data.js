bc00a5e1784d730fd79ac15aad18de6444e4b64a
Aditya Sekhar Bej <adityasekharbj007@gmail.com>
2022-03-30 02:42:07 +0530

    first commit

---
+++ scripts/data.js
@@ -0,0 +1,124 @@
+let offerData = [
+    {
+        title: 'Flat 30% Off on first medicine order',
+        desc: 'Flat 30% Off on first medicine order on minimum cart value Rs.999',
+        img: 'https://cms-contents.pharmeasy.in/offer/203d16e7fc9-FLAT30_off_logo.jpg',
+        code: 'GAIN30',
+        btn: 'Copy Code'
+    },
+    {
+        title: 'Get flat Rs.250 cashback on ICICI Debit & Credit cards',
+        desc: 'Offer valid on transactions above ₹2000.',
+        img: 'https://cms-contents.pharmeasy.in/offer/6901b462e6d-icicinew.jpg',
+        code: '',
+        btn: 'Continue'
+    },
+    {
+        title: 'Flat 20% off + 20% cashback on your 1st medicine order',
+        desc: 'Flat 20% off + Additional 20% cashback upto Rs.300 on your 1st medicine order',
+        img: 'https://cms-contents.pharmeasy.in/offer/4db0adda6b1-40.jpg',
+        code: 'PHEA20',
+        btn: 'Copy Code'
+    },
+    {
+        title: 'Get up to Rs.500 cashback via Amazon Pay',
+        desc: 'Offer valid on transactions above ₹100.',
+        img: 'https://cms-contents.pharmeasy.in/offer/79c9e323814-AMAZON.jpg',
+        code: '',
+        btn: 'Continue'
+    },
+    {
+        title: 'Get FLAT Rs.900 OFF on PharmEasy Lab tests booked above Rs.2998',
+        desc: 'Get FLAT Rs.900 OFF on PharmEasy Lab tests booked above Rs.2998',
+        img: 'https://cms-contents.pharmeasy.in/offer/f99f81a36bd-1X1_Log_PE.png',
+        code: 'GRAND900',
+        btn: 'Copy Code'
+    },
+    {
+        title: 'Get up to Rs.750 cashback via PhonePe wallet only.',
+        desc: 'Offer valid on transactions above ₹500',
+        img: 'https://cms-contents.pharmeasy.in/offer/8500c9729db-logo_PhonePe_PhonePe.png',
+        code: '',
+        btn: 'Continue'
+    },
+    {
+        title: 'Get FLAT Rs.400 cashback on PharmEasy Labtests booked above Rs.949',
+        desc: 'Get FLAT Rs.400 cashback on PharmEasy Labtests booked above Rs.949',
+        img: 'https://cms-contents.pharmeasy.in/offer/f99f81a36bd-1X1_Log_PE.png',
+        code: 'EASY400',
+        btn: 'Copy Code'
+    },
+    {
+        title: 'Get extra 5% Off on Everherb, Liveasy and PharmEasy products',
+        desc: 'Get additional 5% Off on Everherb, Liveasy and PharmEasy products',
+        img: 'https://cms-contents.pharmeasy.in/offer/4d6d03165ea-5per_Off_Logo.jpg',
+        code: 'PL5',
+        btn: 'Copy Code'
+    },
+    {
+        title: 'Get 4000 Paytm Cashback Points via Paytm wallet | Postpaid',
+        desc: 'Valid once per user on transactions above ₹999.',
+        img: 'https://cms-contents.pharmeasy.in/offer/e11270f3e3f-paytmcashbackoptimized.png?dim=60x0&dpr=1.5&q=100',
+        code: '',
+        btn: 'Continue '
+    },
+    {
+        title: 'Get extra 10% Off on Everherb, Liveasy and PharmEasy products',
+        desc: 'Buy any 2 products of Everherb, Liveasy and PharmEasy and get additional 10% Off',
+        img: 'https://cms-contents.pharmeasy.in/offer/60165886431-ten_per.jpg',
+        code: 'ELP10',
+        btn: 'Copy Code'
+    },
+    {
+        title: 'FLAT 18% Off on all medicine orders.',
+        desc: 'Get FLAT 18% Off on all medicine orders + All coupons applicable over & above the flat discount.',
+        img: 'https://cms-contents.pharmeasy.in/offer/df070d160cc-Logo_PE_PE_PE.png?dim=60x0&dpr=1.5&q=100',
+        code: '',
+        btn: 'Continue'
+    },
+    {
+        title: 'Get up to ₹500 cashback via Mobikwik. Use code MBK500 on Mobikiwik',
+        desc: 'Use code MBK500 while paying on Mobikwik, enjoy up to ₹500 cashback on PharmEasy (valid on transactions above ₹750).',
+        img: 'https://cms-contents.pharmeasy.in/offer/8cdf65ac110-mobiwiklogo.png',
+        code: '',
+        btn: 'Continue'
+    },
+    {
+        title: 'Extra 5% off on Optimum Nutrition.',
+        desc: 'Get extra 5% off on Optimum Nutrition Products.',
+        img: 'https://cms-contents.pharmeasy.in/offer/7dda395e2b4-Optimum_Nutrition.jpg',
+        code: 'OPN5',
+        btn: 'Copy Code'
+    },
+    {
+        title: 'Additional 10% off on Savlon',
+        desc: 'Get additional 10% off on Savlon big packs - Savlons expert protection',
+        img: 'https://cms-contents.pharmeasy.in/offer/708966bcfc2-Savlon_new_logo.JPG',
+        code: 'SAVL10',
+        btn: 'Copy Code'
+    },
+    {
+        title: 'Additional 25% off on 2 or more healthcare products',
+        desc: 'Get additional 25% off on 2 or more healthcare products',
+        img: 'https://cms-contents.pharmeasy.in/offer/b00931b6f05-25_Logo.jpg',
+        code: 'BMGM25',
+        btn: 'Copy Code'
+    },
+    {
+        title: 'Extra 25% off on select Kapiva Products',
+        desc: 'Extra 25% off on select Kapiva Products',
+        img: 'https://cms-contents.pharmeasy.in/offer/fe63bbf8714-kapiv11.jpg',
+        code: 'KAPIVA25',
+        btn: 'Copy Code'
+    },
+    {
+        title: 'Get FLAT Rs.499 cashback on PharmEasy Labtests booked above Rs.499',
+        desc: 'Get FLAT Rs.499 cashback on PharmEasy Labtests booked above Rs.499',
+        img: 'https://cms-contents.pharmeasy.in/offer/f99f81a36bd-1X1_Log_PE.png',
+        code: 'GETCSH',
+        btn: 'Copy Code'
+    }
+    
+];
+
+localStorage.setItem('offers', JSON.stringify(offerData))
