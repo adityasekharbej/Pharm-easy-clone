7358f397465a3d7b899bcb38cf37c99063187dce
Aditya Sekhar Bej <95712637+adityasekharbej@users.noreply.github.com>
2022-04-02 15:19:10 +0530

    first commit

--- scripts/cart_header.js
+++
@@ -1,8 +0,0 @@
-const enterOTP=()=>{
-    let bt=document.getElementById("loginuser");
-    bt.innerText="Proceed to Payment";
-    bt.addEventListener("click",()=>{
-        window.location.href="payment.html";
-    })
-    closeNav()
-}
